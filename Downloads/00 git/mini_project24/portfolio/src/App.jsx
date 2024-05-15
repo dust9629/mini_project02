92737ead4948cf046789a7ed2f4fcee7dee99dd4
EunJi Kim <dust9629@gmail.com>
2024-05-15 23:46:18 +0900

    Initial commit

---
+++ Downloads/00 git/mini_project24/portfolio/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Axios from "axios";
+
+function App() {
+  return (
+    <div className="App">
+      <div>hello</div>
+    </div>
+  );
+}
+
+export default App;

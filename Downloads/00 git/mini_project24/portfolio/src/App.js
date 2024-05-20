dac9b99f77455386beee92bdde5dca50a836e94e
EunJi Kim <dust9629@gmail.com>
2024-05-20 23:33:01 +0900

    modify main css

--- Downloads/00 git/mini_project24/portfolio/src/App.js
+++ Downloads/00 git/mini_project24/portfolio/src/App.js
@@ -9,6 +9,8 @@ function App() {
   // const [username, setUsername] = useState("");
   // const { isOn, toggle } = useToggle(false);
   let [like, likeCount] = useState(0);
+  let [modal, setModal] = useState(false);
+
   function likeFn(current) {
     likeCount(like++);
   }
@@ -30,6 +32,7 @@ function App() {
       >
         {isOn === true ? "on" : "off"}
       </button> */}
+      {/* 상단 menu */}
       <nav>
         <ul className="nav_wrap left">
           <li>Contact</li>
@@ -39,47 +42,81 @@ function App() {
           <li>A-Z</li>
         </ul>
       </nav>
+      <div className="mo_nav_wrap">
+        <p
+          onClick={() => {
+            setModal(true);
+          }}
+        >
+          menu
+        </p>
+        {modal == true ? <NavMobile /> : null}
+      </div>
 
-      <main className="main">
-        <div className="section01">
-          <h1>PORTFOLIO</h1>
-          <div className="contents"></div>
-          <div className="like">
-            좋아요👍
-            <span onClick={likeFn}> {like}</span>
-          </div>
+      {/* 메인_01 */}
+      <div className="section01">
+        <div className="main_txt">
+          <h1>Front-end</h1>
         </div>
+        <div className="contents"></div>
+        {/* <div className="like">
+          좋아요👍
+          <span onClick={likeFn}> {like}</span>
+        </div> */}
+      </div>
 
-        <div className="section02">
-          <h3>Contents</h3>
-          <ul>
-            <li>{title[0]}</li>
-            <li>{title[1]}</li>
-            <li>{title[2]}</li>
-          </ul>
-          <button
-            onClick={() => {
-              let titleChg = [...title];
-              titleChg[0] = "이커머스";
-              titleList(titleChg);
-            }}
-          >
-            click
-          </button>
+      {/* 메인_02 */}
+      <div className="section02">
+        <h2>Contents</h2>
+        <ul>
+          {/* <li>{title[0]}</li>
+          <li>{title[1]}</li>
+          <li>{title[2]}</li> */}
 
-          <Contents></Contents>
-        </div>
-      </main>
+          {title.map(function (a, i) {
+            return (
+              <li key={i}>
+                {i + 1}.{a}
+              </li>
+            );
+          })}
+        </ul>
+        <button
+          onClick={() => {
+            let titleChg = [...title];
+            titleChg[0] = "이커머스";
+            titleList(titleChg);
+          }}
+        >
+          click
+        </button>
+      </div>
+
+      {/* 메인_03 */}
+      <div className="section03">
+        <h3>Color Palette</h3>
+      </div>
     </div>
   );
 
   function Contents() {
+    let [project, setProject] = useState(["카페24", "솔루션"]);
     return (
-      <div className="conts_wrap">
+      <div className="cont_box">
         <h4 className="cont_tit">솔루션</h4>
-        <p>카페24</p>
+        <div>{project[0]}</div>
       </div>
-    )
+    );
+  }
+
+  function NavMobile() {
+    return (
+      <ul className="mo_nav">
+        <li>Contact</li>
+        <li>Team_project</li>
+        <li>A-Z</li>
+      </ul>
+    );
   }
 }
 
